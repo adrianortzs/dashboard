26ad0d61dc54b52aa90c0ba431bcdff418c3f31e
adrianortzs <adrianortizsuarez@icloud.com>
2024-04-05 14:29:22 +0200

    feat: localStorage

---
+++ script/links.js
@@ -0,0 +1,26 @@
+const botonAñadir = document.querySelector("button")
+const section = document.querySelector("section")
+
+let contador = 0
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+})
+
+botonAñadir.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    contador ++
+    
+    const link = document.getElementById("link").value
+    const nombreGuardado = document.getElementById("nombre-guardado").value
+    const valores = {nombre: nombreGuardado, link: link}
+    localStorage.setItem(`Link ${contador}`, JSON.stringify(valores))
+    
+    section.innerHTML += `
+    <ul>
+    <li>${nombreGuardado}</li>
+    <li>${link}</li>
+    <li><button class="borrar-link">Borrar</button></li>
+    </ul>`
+})
