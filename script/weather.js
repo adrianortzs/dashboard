d6fb6bfb502dd312db73f8f16d50532bfbb579ae
adrianortzs <adrianortizsuarez@icloud.com>
2024-04-10 14:26:38 +0200

    feat:fetch datos

--- script/weather.js
+++ script/weather.js
@@ -1,12 +1,31 @@
 const apiKey = "e2053aba2498460e86e160942240804"
+const city = "Madrid"
 
-async function fetchApi() {
+async function fetchUbicacion() {
     try {
-        const response = await fetch(`https://www.weatherapi.com/v1`)
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&aqi=no`)
         const data = await response.json()
-        console.log(data)
-    }
-    catch {
 
+        const datosCiudad = {
+                ciudad: data.location.region,
+                pais: data.location.country
+            }
+
+        const datosTiempo = {
+                estado: data.current.condition.text,
+                imagenEstado: data.current.condition.icon,
+                grados: `${data.current.temp_c}°`,
+                precipitaciones: data.current.precip_in,
+                humedad: data.current.humidity,
+                velocidadViento: data.current.wind_kph
+            }
+
+        console.log(datosCiudad)
+        console.log(datosTiempo)
+    }
+    catch(error) {
+        console.error('Error fetching countries:', error)
     }
 }
+
+fetchUbicacion()
